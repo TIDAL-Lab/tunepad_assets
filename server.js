d9f0ac2ad3bccc95a7328b6c798518f7cf9aebbc
Michael Horn <michael-horn@northwestern.edu>
2025-12-12 00:36:43 -0600

    cross origin

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const cors = require('cors')
 const PORT = 3030;
 
-// Serve static files from the "latest" directory
+app.use(cors())
 app.use(express.static(path.join(__dirname, 'latest')));
-
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
